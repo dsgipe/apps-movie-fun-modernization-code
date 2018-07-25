9cb63a6862942dc931b5473bd34fed9735c0f7b0
jothimani-rajendran <jothimani.rajendran@gmail.com>
2018-07-25 14:25:24 -0600

    wip microservices added files

---
+++ components/movie-fun-api/src/main/java/org/superbiz/moviefun/albumsapi/AlbumsClient.java
@@ -0,0 +1,39 @@
+package org.superbiz.moviefun.albumsapi;
+
+import org.springframework.web.client.RestOperations;
+import org.superbiz.moviefun.moviesapi.MovieInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumsClient {
+    private final RestOperations restOperations;
+    private final String albumServerEndpoint;
+
+    public AlbumsClient(String movieServerEndpoint,RestOperations restOperations) {
+        this.restOperations = restOperations;
+        this.albumServerEndpoint = movieServerEndpoint;
+    }
+
+    public void addAlbum(AlbumInfo album) {
+        restOperations.postForEntity(albumServerEndpoint, album,AlbumInfo.class);
+    }
+
+    public AlbumInfo find(long id) {
+        return restOperations.getForObject(albumServerEndpoint+"/"+id,AlbumInfo.class);
+
+    }
+
+    public List<AlbumInfo> getAlbums() {
+        return restOperations.getForObject(
+                albumServerEndpoint, new ArrayList<AlbumInfo>().getClass());
+    }
+
+    public void deleteAlbum(AlbumInfo album) {
+        restOperations.delete(albumServerEndpoint, album);
+    }
+
+    public void updateAlbum(AlbumInfo album) {
+        restOperations.put(albumServerEndpoint, album);
+    }
+}
